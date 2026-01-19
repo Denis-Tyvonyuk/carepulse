e3a590cd45927450563d7eb0c6fc31a12098a346
Denis-Tyvonyuk <denusa321@gmail.com>
2026-01-19 22:43:52 +0200

    assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
 import Image from "next/image";
+import PatientForm from "../components/forms/PatientForm";
 
 export default function Home() {
-  return <div className="text-amber-300 bg-blue-500">home</div>;
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container msx-w-[496px]">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            height={100}
+            width={100}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              ~ 2026 CarePulse
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
